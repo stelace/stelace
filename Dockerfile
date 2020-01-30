stelace/stelace

commit c620022fcef6824a641db4dc6509904dd1387ee9

chore!: bump node version to v12.14
preparing for ava v3 upgrade

Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.7-alpine
+FROM node:12.14-alpine
 
 # Puppeteer installation process inspired by:
 # https://github.com/GoogleChrome/puppeteer/issues/1793#issuecomment-442730223

Dockerfile.prod
@@ -2,7 +2,7 @@
 # Using BuildKit for SSH, please refer to
 # https://docs.docker.com/develop/develop-images/build_enhancements/#using-ssh-to-access-private-data-in-builds
 
-FROM node:12.7-alpine
+FROM node:12.14-alpine
 
 # Puppeteer installation process inspired by:
 # https://github.com/GoogleChrome/puppeteer/issues/1793#issuecomment-442730223

.circleci/config.yml
@@ -34,8 +34,8 @@ jobs:
             curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
             export NVM_DIR="/opt/circleci/.nvm"
             [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-            nvm install v12.7.0
-            nvm alias default v12.7.0
+            nvm install v12.14.0
+            nvm alias default v12.14.0
             # Each step uses the same `$BASH_ENV`, so need to modify it
             echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
             echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV

src/util/intl.js
@@ -1,8 +1,8 @@
-// With current version (12.7) of Node.js, `small-icu` is embedded by default
+// With current version (12.14) of Node.js, `small-icu` is embedded by default
 // (only English support for dates and numbers)
 // https://nodejs.org/api/intl.html#intl_options_for_building_node_js
 
-// There is a plan to include `full-icu` by default in a future version
+// `full-icu` is included by default starting from v13
 // https://github.com/nodejs/node/pull/29522
 
 // For the time being, we need to apply a polyfill to support all locales.

commit 8d2258859e52becc5b8524f7af52ee3f6e2441fc

fix: broken commands after #138

.env.example
@@ -33,7 +33,8 @@ INSTALLED_PLUGINS=https://github.com/stelace/stelace-search-filter-dsl-parser.gi
 # Set host to 'redis' and change port if needed when using dockerized API
 REDIS_PORT=6379
 REDIS_HOST=127.0.0.1
-# REDIS_HOST=redis # when using Stelace API server docker container
+# When using Stelace API server docker container:
+# REDIS_HOST=redis
 REDIS_DBNUM=0
 REDIS_PASSWORD=
 REDIS_TLS=false
@@ -42,9 +43,10 @@ REDIS_TLS=false
 # Otherwise docker-compose file maps from 5432 PostgreSQL default port number in container
 # to 6543 host port in order to avoid conflicts since PostgreSQL often ships with OS.
 POSTGRES_HOST=127.0.0.1
-# POSTGRES_HOST=postgresql # when using Stelace API server docker container
 POSTGRES_PORT=6543
-# POSTGRES_PORT=5432 # when using Stelace API server docker container
+# When using Stelace API server docker container:
+# POSTGRES_HOST=postgresql
+# POSTGRES_PORT=5432
 POSTGRES_DB=stelace-core
 POSTGRES_USER=user
 POSTGRES_PASSWORD=password
@@ -52,7 +54,8 @@ POSTGRES_ADMIN_USER=postgres
 
 # Set host to 'elasticsearch' when using dockerized API
 ELASTIC_SEARCH_HOST=127.0.0.1
-# ELASTIC_SEARCH_HOST=elasticsearch # when using Stelace API server docker container
+# When using Stelace API server docker container:
+# ELASTIC_SEARCH_HOST=elasticsearch
 ELASTIC_SEARCH_PROTOCOL=http
 ELASTIC_SEARCH_PORT=9200
 ELASTIC_SEARCH_USER=elastic

Dockerfile
@@ -19,4 +19,4 @@ WORKDIR /usr/src/app
 
 USER node
 
-CMD [ "node", "start.js" ]
+CMD [ "node", "server/start.js" ]

Dockerfile.prod
@@ -36,4 +36,4 @@ COPY . /usr/src/app
 WORKDIR /usr/src/app
 RUN --mount=type=secret,id=env,dst=/usr/src/app/.env yarn plugins:prepare
 
-CMD [ "node", "start.js" ]
+CMD [ "node", "server/start.js" ]

server/start.js
@@ -6,7 +6,7 @@ const { start: startApm } = require('./apm')
 // start the APM service before Restify server so it can monitor all application lifecycle
 startApm()
 
-const { start, stop } = require('./server')
+const { start, stop } = require('./index')
 const { onClose } = require('./close')
 
 start()