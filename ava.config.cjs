8f7ca1bc9d347f334915502e3421cfbb47d55904
Wing-On Yuen <25850444+woyuen@users.noreply.github.com>
2020-05-25 17:02:02 +0200

    Fix: seeding was broken due to update to ava@3 (#295)
    fix: broken seed script
    
    due to update to ava@3

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { IGNORED_LOCAL_PLUGINS, TESTS, STELACE_PLUGINS_PATHS } = process.env
+const { IGNORED_LOCAL_PLUGINS, TESTS, STELACE_PLUGINS_PATHS, INSTANT_DATA } = process.env
 
 const fromEnvVar = (v = '') => v.split(',').filter(Boolean).map(s => s.trim())
 
@@ -8,18 +8,26 @@ const pluginTestsToIgnore = fromEnvVar(IGNORED_LOCAL_PLUGINS).map(p => `!plugins
 const cliLoadedPluginTests = fromEnvVar(STELACE_PLUGINS_PATHS).map(p => `${p}/**/*.spec.js`)
 
 let tests = []
+const seedTests = ['scripts/instantData.js']
 const integrationTests = ['test/integration/**/*.spec.js', 'plugins/**/*.spec.js', ...cliLoadedPluginTests]
 const unitTests = ['test/unit/**/*.spec.js']
 
-if (TESTS === 'integration') tests.push(...integrationTests)
-else if (TESTS === 'unit') tests.push(...unitTests)
-else tests = [...unitTests, ...integrationTests]
+let files = []
 
-module.exports = () => ({
-  files: [
+if (INSTANT_DATA === 'true') files = [...seedTests]
+else {
+  if (TESTS === 'integration') tests.push(...integrationTests)
+  else if (TESTS === 'unit') tests.push(...unitTests)
+  else tests = [...unitTests, ...integrationTests]
+
+  files = [
     ...tests,
     ...pluginTestsToIgnore
-  ],
+  ]
+}
+
+module.exports = () => ({
+  files,
   serial: false,
   cache: false,
   timeout: '30s'
